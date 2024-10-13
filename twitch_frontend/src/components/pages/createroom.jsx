b0ed791b3fe5e385455c78dcfc94f8a330106f3d
04maheshr <r.hsirah.1@gmail.com>
2024-10-13 17:56:00 +0530

    changed navigate

--- twitch_frontend/src/components/pages/createroom.jsx
+++ twitch_frontend/src/components/pages/createroom.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaPlusCircle } from "react-icons/fa";
 
 const HomePage = () => {
   const [showIcon, setIcon] = useState(false);
+  const navigate = useNavigate();
 
   const toggleFunction = () => {
     setIcon((prev) => !prev);
